eef14c0ce6d2bb0869e2756da71606c475dbbf7e
rajalamdev <rajalamofficial@gmail.com>
2023-08-07 22:45:55 +0700

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        'bg-outside': 'var(--bg-outside)',
+        'bg-primary': 'var(--bg-primary)',
+        'line': 'var(--line)'
+      }
     },
   },
   plugins: [],
