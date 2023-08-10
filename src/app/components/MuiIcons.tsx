87b5d0bc7d55cd4e30a9054f7aa2430bfee05994
rajalamdev <rajalamofficial@gmail.com>
2023-08-10 15:22:41 +0700

    test: deploy the application to test the performance

---
+++ src/app/components/MuiIcons.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from "react"
+import * as Icons from "@mui/icons-material"
+
+type IconNames = keyof typeof Icons
+type IconProps = {
+  iconName: IconNames
+}
+
+const MuiIconsComponent: FC<IconProps> = ({
+  iconName
+}) => {
+  const Icon = Icons[iconName]
+  return <Icon />
+}
+
+export default MuiIconsComponent
--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,10 +1,16 @@
+"use client"
+
+import MuiIconsComponent from "../MuiIcons";
+
 const Footer = () => {
   return (
-    <footer className="border-t border-line absolute bottom-0 w-full flex justify-between z-10 bg-bg-primary">
+    <footer className="border-t  border-line absolute bottom-0 w-full flex justify-between z-10 bg-bg-primary">
       <div className="flex">
         <h4 className="px-6 py-3 border-r border-line">find me in: </h4>
-        <div className="px-4 py-3 border-r border-line button-hover">sdf</div>
-        <div className="px-4 py-3 border-r border-line button-hover">sdf</div>
+        <div className="px-4 py-3 flex justify-center items-center border-r border-line button-hover">
+          <MuiIconsComponent iconName="Add" />
+        </div>
+        <div className="px-4 py-3 border-r  border-line button-hover">sdf</div>
       </div>
       <div></div>
     </footer>
