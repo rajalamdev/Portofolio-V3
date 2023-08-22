feed20a3e1743521afe01f8dd1481bf821a6ad26
rajalamdev <rajalamofficial@gmail.com>
2023-08-23 01:41:09 +0700

    feat(api): add now playing spotify api

---
+++ src/app/api/now-playing/route.ts
@@ -0,0 +1,31 @@
+import { NextRequest, NextResponse } from 'next/server'
+import { headers } from 'next/headers'
+import { getNowPlaying } from '@/lib/spotify';
+ 
+export async function GET(request: NextRequest, res: NextResponse) {
+  const response = await getNowPlaying();
+
+  if (response.status === 204 || response.status > 400) {
+    return NextResponse.json({ isPlaying: false }, { status: 200})
+  }
+ 
+  const song = await response.json()
+  const isPlaying = song.is_playing
+  const title = song.item.name
+  const artist = song.item.artists.map((_artist:any) => _artist.name).join(', ')
+  const album = song.item.album.name
+  const albumImageUrl = song.item.album.images[0].url
+  const songUrl = song.item.external_urls.spotify
+
+
+
+  return NextResponse.json({
+    album,
+    albumImageUrl,
+    artist,
+    isPlaying,
+    songUrl,
+    title,
+    song,
+  })
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,16 @@
+import { getNowPlaying } from "@/lib/spotify"
 import Layout from "../components/layout/Layout"
 
-const Blog = () => {
+const getNowPlayingSpotify = async () => {
+  const res = await fetch("http://localhost:3000/api/now-playing")
+  return await res.json()
+
+}
+
+const Blog = async () => {
+  const getRes = await getNowPlayingSpotify();
+  console.log(getRes)
+
   return (
     <div></div>
   )
