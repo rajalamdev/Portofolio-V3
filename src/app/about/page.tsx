127aed4655a31c7da3b5361f984e78dead2f637a
rajalamdev <rajalamofficial@gmail.com>
2023-08-23 23:39:04 +0700

    test: testing

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -23,7 +23,7 @@ const fetcher = (url: string) => fetch(url).then(res => res.json())
 const About = () => {
   const [personalInfoActive, setPersonalInfoActive] = useState(true)
   const [contactActive, setContactActive] = useState(true)
-  const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_BASE_URL}/api/now-playing`, fetcher)
+  const { data, error, isLoading } = useSWR(`https://rajalam-dev.vercel.app/api/now-playing`, fetcher)
   const section = useRef()
 
   const [folders, setFolders] = useState([
