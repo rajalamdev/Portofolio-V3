3686373eafaafc7835fe948d1266a0ec05e23416
rajalamdev <rajalamofficial@gmail.com>
2023-08-31 22:41:24 +0700

    feat: add filter projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -5,15 +5,12 @@ import Link from "next/link"
 import { useEffect, useState } from "react"
 import useSWR from "swr"
 
-// query
-// http://localhost:1337/api/projects?populate[project_categories][populate]=icons&filters[project_categories][query][$contains]=next-js
-
 const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 const Projects =  () => {
   const [queryLanguage, setQueryLanguage] = useState<any>([])
-  const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/projects?populate[image]=*&populate[project_categories][populate]=icons${queryLanguage.map(query => `&filters[project_categories][query][$in]=${query}`)}`, fetcher)
-  console.log(data)
+  const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_STRAPI_URL}/api/projects?populate[image]=*&populate[project_categories][populate]=icons${queryLanguage.map((q: string, index: number) => `&filters[$and][${index}][project_categories][query][$contains]=${q}`).join("")}`, fetcher)
+
 
   const [filterLanguage, setFilterLanguage] = useState([
     {name: "next-js", icon: "nextjs", active: false},
@@ -22,15 +19,15 @@ const Projects =  () => {
     {name: "vue-js", icon: "react", active: false},
     {name: "react-native", icon: "react", active: false},
   ])
- 
+
   // &filters[project_categories][query][$contains]=tailwind&filters[project_categories][query][$contains]=nextjs
   function filteredLanguageHandler(currentLanguage: any){
-    if(!queryLanguage.some(lang => lang === currentLanguage.name)){
+    if(!queryLanguage.some((query: string) => query === currentLanguage.name)){
       setQueryLanguage([...queryLanguage, currentLanguage.name])
     }
     
-    if(queryLanguage.some(lang => lang === currentLanguage.name)){
-      const filteredQuery = queryLanguage.filter(query => query != currentLanguage.name)
+    if(queryLanguage.some((query: string) => query === currentLanguage.name)){
+      const filteredQuery = queryLanguage.filter((query: string) => query != currentLanguage.name)
       setQueryLanguage(filteredQuery)
     }
 
@@ -66,7 +63,7 @@ const Projects =  () => {
         {isLoading && <p>Loading...</p>}
         {!isLoading && (
           <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-            {data.data.map(project => {
+            {data.data.map((project: any) => {
               return (
                 <Link href={`/projects/${project.attributes.slug}`} className="border border-line p-4 rounded flex flex-col gap-4 hover:scale-[1.02] cursor-pointer transition-transform">
                   <div className="space-y-1 flex-1">
@@ -75,12 +72,12 @@ const Projects =  () => {
                   </div>
                   <div className="space-y-4 flex-1">
                     <div className="w-full h-[180px] relative rounded overflow-hidden">
-                      <Image src={project.attributes.image.data.attributes.formats.medium.url} fill className="object-cover" alt="project image" />
+                      <Image src={project.attributes.image.data[0].attributes.formats.medium.url} fill className="object-cover" alt="project image" />
                       <div className="flex justify-end gap-2 flex-wrap absolute top-0 right-0 p-2">
-                        {project.attributes.project_categories.data.map(icon => {
+                        {project.attributes.project_categories.data.map((icon: any) => {
                           return (
                             <div className="p-1 flex gap-2 bg-accent">
-                              <DynamicSvgIcon name={icon.attributes.icons.data[0].attributes.name} className="w-4 fill-black" />
+                              <DynamicSvgIcon name={icon.attributes.icons.data[0].attributes.title} className="w-4 fill-black" />
                             </div>
                           )
                         })}
