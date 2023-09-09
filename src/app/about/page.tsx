8cc6e4e1be43a5c3fb058d6d7e97a19cb19aaefe
rajalamdev <rajalamofficial@gmail.com>
2023-09-09 18:37:56 +0700

    test: testing the Spotify API

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -20,12 +20,12 @@ import SpotifyNowPlayingSkeleton from "@/components/loading-skeleton/SpotifyNowP
 //   },
 // }
 
-const fetcher = (url: string) => fetch(url).then(res => res.json())
+// const fetcher = (url: string) => fetch(url).then(res => res.json())
 
 const About = () => {
   const [personalInfoActive, setPersonalInfoActive] = useState(true)
   const [contactActive, setContactActive] = useState(true)
-  const { data, error, isLoading } = useSWR(`${process.env.NEXT_PUBLIC_BASE_URL}/api/now-playing`, fetcher)
+  const { data, error, isLoading } = useSWR(`/api/now-playing`)
 
   const [folders, setFolders] = useState([
     {
